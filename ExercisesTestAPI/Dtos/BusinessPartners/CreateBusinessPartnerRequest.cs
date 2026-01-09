9b2ff8228ff5995f6ec459af03977c3ac513c73c
Alexandros Giannakis <alexandros.giannakis@gvision.gr>
2026-01-09 10:22:39 +0200

    updated CreateBusinessPartnerRequest

--- ExercisesTestAPI/Dtos/BusinessPartners/CreateBusinessPartnerRequest.cs
+++ ExercisesTestAPI/Dtos/BusinessPartners/CreateBusinessPartnerRequest.cs
@@ -4,8 +4,11 @@ public sealed class CreateBusinessPartnerRequest
 {
     public string CardCode { get; set; } = default!;
     public string CardName { get; set; } = default!;
-    public string CardType { get; set; } = "cCustomer";
+    public string CardType { get; set; } = "cCustomer"!;
     public string? FederalTaxID { get; set; } = default!;
     public string? BilltoDefault { get; set; } = default!;
     public string? ShipToDefault { get; set; } = default!;
+    public List<BPAddressDto> BPAddresses { get; set; } = new List<BPAddressDto>();
+    public List<ContactEmployeesDto> ContactEmployees { get; set; } = new List<ContactEmployeesDto>();
+
 }
